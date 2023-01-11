3540c75feb2de46e92ab8109e14acd7333730d76
MeriemBoujamza <kazahayakun@gmail.com>
2023-01-11 10:03:50 +0100

    initial commit

---
+++ src/main/java/fr/ensim/info/zoo/Tp1BoujamzaApplication.java
@@ -0,0 +1,13 @@
+package fr.ensim.info.zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1BoujamzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1BoujamzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ensim/info/zoo/Tp1BoujamzaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ensim.info.zoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1BoujamzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
